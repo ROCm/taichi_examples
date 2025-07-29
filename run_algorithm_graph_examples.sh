40f68ef241a9e47053b56051cc6fbaaa873d4be6
tmm77 <6461957+tmm77@users.noreply.github.com>
2025-07-29 11:22:38 -0400

    removing '/app' from directory path

--- run_algorithm_graph_examples.sh
+++ run_algorithm_graph_examples.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd /app/examples
+cd examples
 python3 stable_fluid_graph.py
 
-cd /app/examples/algorithms
+cd examples/algorithms
 python3 laplace.py
 python3 poisson_disk_sampling.py
 
