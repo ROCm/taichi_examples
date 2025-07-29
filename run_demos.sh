150066362039e11b2b8fa423a2dd32a76d4cfeec
tmm77 <6461957+tmm77@users.noreply.github.com>
2025-07-29 11:22:19 -0400

    removing '/app' from directory path

--- run_demos.sh
+++ run_demos.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd /app/examples
+cd examples
 python3 lcs.py
 python3 count_primes.py  
 python3 fractal.py  
 python3 physical_simulation.py
 
-cd /app/examples/demo_taichi
+cd examples/demo_taichi
 python3 demo_taichi.py
 
